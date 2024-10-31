f2c9a4446747f8031535e91c37adce6fb634bd5c
Christopher Robison <cdr@netoasis.net>
2024-10-31 08:44:23 -0700

    Adding deploy script

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-soroban contract deploy \
-  --wasm target/wasm32-unknown-unknown/release/notary_contract.wasm \
+stellar contract deploy \
+  --wasm target/wasm32-unknown-unknown/release/notary.wasm \
   --network testnet \
-  --source SBPW2BGMHBFKVBZUBLPVDVWI42P4CC2LHYGQSGDHLF7AUOARPNNMKU33
+  --source docchain
